dbd1bab23d911ef1c72431eeff857df867e8b8f3
Joseph Kamau <muigaijosephkamau@gmail.com>
2019-10-13 14:52:37 +0300

    created a function configure_request()

--- app/request.py
+++ app/request.py
@@ -11,3 +11,10 @@ base_url = None
 
 # Article URL
 article_url = None
+
+def configure_request(app):
+    global api_key, base_url, article_url
+    api_key = app.config['NEWS_API_KEY']
+    # print(api_key)
+    base_url = app.config['NEWS_API_BASE_URL']
+    article_url = app.config['BASE_URL']
