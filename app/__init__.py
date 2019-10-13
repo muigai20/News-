c6a5bafe0f7b52b2fd36dded95aa9a5dd9d9cf0f
Joseph Kamau <muigaijosephkamau@gmail.com>
2019-10-13 12:09:20 +0300

    created the app configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from config import config_options
 
-#initializing application
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # create the app configurations
+    app.config.from_object(config_options[config_name])
