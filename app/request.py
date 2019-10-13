b5cbe1601100d4da870a12834ec9bd18648b00c9
Joseph Kamau <muigaijosephkamau@gmail.com>
2019-10-13 14:50:33 +0300

    created api_key,base_url and article_url variables and set them to none

--- app/request.py
+++ app/request.py
@@ -3,3 +3,11 @@ from .models import Sources, Article
 
 # Sources = news.Sources
 Articles = Article
+# Getting the api key
+api_key = None
+
+# Base URL
+base_url = None
+
+# Article URL
+article_url = None
