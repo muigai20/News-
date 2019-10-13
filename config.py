7ee9361a0709cc67ad660d6ecb9a687f67e511de
Joseph Kamau <muigaijosephkamau@gmail.com>
2019-10-13 14:02:19 +0300

    updated the base and news.html files

--- config.py
+++ config.py
@@ -4,7 +4,10 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
 
 
 class ProdConfig(Config):
